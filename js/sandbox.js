175ef92fa73e97c974a139691b61d249be60cb77
Esteban Vihaio <a.de.ruijter@mboutrecht.nl>
2017-12-15 14:04:34 +0100

    week6 vrijdag 15-12-2017

--- js/sandbox.js
+++ js/sandbox.js
@@ -23,12 +23,23 @@ function printName() {
 // **********************************************************************
 var fruit = ["banaan", "sinaasappel", "meloen", "peer"];
 
+function array_ordered_list() {
+  var output = "<ol>";
+  for ( var i = 0; i < fruit.length; i++) {
+    output += "<li>" + fruit[i] + "</li>";
+  }
+  output += "</ol>";
+  return output;
+}
+
+// https://www.w3schools.com/js/js_array_methods.asp
+
 function show_array() {
   // alert("Hoi");  
   console.log(fruit);
   var p_tag = document.getElementById("outputArray");
   // console.log(p_tag.outerHTML);
-  p_tag.innerHTML = "De inhoud van mijn fruit-array is: " + fruit;
+  p_tag.innerHTML = "De inhoud van mijn fruit-array is: " + array_ordered_list();
 }
 
 function fruit_toevoegen() {
@@ -41,7 +52,15 @@ function fruit_toevoegen() {
   output_fruit_toevoegen.innerHTML = fruit;
 }
 
+var dbl_button = document.getElementById("dbl_button");
+dbl_button.innerHTML = array_ordered_list();
 
+function add_array_bottom() {
+  var input_dbl_button = document.getElementById("input_dbl_button");
+  fruit.push(input_dbl_button.value);
+  dbl_button.innerHTML = array_ordered_list();
+  input_dbl_button.value = "";
+}
 
 // Einde section output (#js03)
 // *************************************************************************
